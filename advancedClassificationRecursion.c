b69917956772841f7cb8db55acd65f043c2223d0
shirelzz <99799701+shirelzz@users.noreply.github.com>
2022-11-18 13:36:12 +0200

    adding return statement to reversedNum

--- advancedClassificationRecursion.c
+++ advancedClassificationRecursion.c
@@ -5,58 +5,46 @@
 #define FALSE 0
 
 // Utility function
-int numOfDigits(int x)
-{
-    if (x / 10 == 0)
-    {
+int numOfDigits(int x){
+    if (x / 10 == 0){
         return 1;
     }
-    else
-    {
+    else{
         return 1 + numOfDigits(x / 10);
     }
 }
 
 // Utility function
-int calcSum(int x, int n)
-{
-    if (x / 10 == 0)
-    {
+int calcSum(int x, int n){
+    if (x / 10 == 0){
         return (int)pow(x, n);
     }
-    else
-    {
+    else{
         return (int)(pow(x % 10, n)) + calcSum(x / 10, n);
     }
 }
 
-int isArmstrong(int x)
-{
+int isArmstrong(int x){
     int n = numOfDigits(x);
-    if (calcSum(x, n) == x)
-    {
+    if (calcSum(x, n) == x){
         return TRUE;
     }
-    else
-    {
+    else{
         return FALSE;
     }
 }
 
-int isPalindromeReverse(int num1, int num2)
-{
-    if (num1 == 0)
-    {
+// Utility function
+int reversedNum(int num1, int num2){
+    if (num1 == 0){
         return num2;
     }
-    isPalindromeReverse((num1 / 10), ((num2 * 10) + (num1 % 10)));
+    return reversedNum((num1 / 10), ((num2 * 10) + (num1 % 10)));
 }
 
-int isPalindrome(int num)
-{
+int isPalindrome(int num){
     int reversed_num = isPalindromeReverse(num, 0);
-    if (num == reversed_num)
-    {
+    if (num == reversed_num){
         return TRUE;
     }
     return FALSE;
